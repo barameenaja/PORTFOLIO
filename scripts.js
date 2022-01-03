63e16c7a55cf5a093af123c994c0bd2808b97ead
Louizzz <96277906+barameenaja@users.noreply.github.com>
2022-01-03 21:10:13 +0700

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,14 @@
+const Tap = document.querySelector("ul")
+
+function switchMode(){ 
+    Tap.classList.toggle("nav-active")
+}
+
+const Tap1 = document.querySelector(".container1")
+
+function switchMode1(){ 
+    Tap1.classList.toggle("container1-2")
+}
+
+//.container1
+//container1-2
